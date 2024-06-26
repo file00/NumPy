f464bc2f9036b0dcf61f401fff326c92f51590c6
file00 <51076009+file00@users.noreply.github.com>
2024-06-26 13:38:17 +0900

    Add files via upload

---
+++ NumPy Array Creation/Create an Array With np.random.rand().py
@@ -0,0 +1,7 @@
+# Create an Array With np.random.rand()
+import numpy as np
+
+# generate an array of 5 random numbers
+array1 = np.random.rand(5)
+
+print(array1)
---
+++ NumPy Array Creation/Create an Empty NumPy Array.py
@@ -0,0 +1,7 @@
+# Create an Empty NumPy Array
+import numpy as np
+
+# create an empty array of length 4
+array1 = np.empty(4)
+
+print(array1)
