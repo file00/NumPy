ee15d0daad9661b3bcea949d499d279875368416
file00 <51076009+file00@users.noreply.github.com>
2024-07-19 14:47:33 +0900

    Delete NumPy N-D Array Creation directory

--- NumPy N-D Array Creation/Create N-D Array with a Specified Value.py
+++
@@ -1,7 +0,0 @@
-# Create N-D Array with a Specified Value
-import numpy as np
-
-# Create a 2-D array with elements initialized to 5 
-numpy_array = np.full((2, 2), 5)
-
-print("Array:", numpy_array)
--- NumPy N-D Array Creation/Create a 2-D NumPy Array.py
+++
@@ -1,8 +0,0 @@
-# Create a 2-D NumPy Array
-import numpy as np
-
-# create a 2D array with 2 rows and 4 columns
-array1 = np.array([[1, 2, 3, 4],
-                  [5, 6, 7, 8]])
-
-print(array1)
