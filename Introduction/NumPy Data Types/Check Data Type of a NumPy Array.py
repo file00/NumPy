8bed96b5fcbda4ee149fdd5635c43cf04fb241d7
file00 <51076009+file00@users.noreply.github.com>
2024-07-19 14:52:27 +0900

    Add files via upload

---
+++ Introduction/NumPy Data Types/Check Data Type of a NumPy Array.py
@@ -0,0 +1,11 @@
+# Check Data Type of a NumPy Array
+
+import numpy as np
+
+# create an array of integers 
+array1 = np.array([2, 4, 6])
+
+# check the data type of array1
+print(array1.dtype) 
+
+# Output: int64
