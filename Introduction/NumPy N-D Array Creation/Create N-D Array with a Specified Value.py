e92361a2cb2466327a8734ed0a7f00fa59c433d7
file00 <51076009+file00@users.noreply.github.com>
2024-07-19 14:47:49 +0900

    Add files via upload

---
+++ Introduction/NumPy N-D Array Creation/Create N-D Array with a Specified Value.py
@@ -0,0 +1,7 @@
+# Create N-D Array with a Specified Value
+import numpy as np
+
+# Create a 2-D array with elements initialized to 5 
+numpy_array = np.full((2, 2), 5)
+
+print("Array:", numpy_array)
---
+++ Introduction/NumPy N-D Array Creation/Creating Arrays With np.random.rand().py
@@ -0,0 +1,14 @@
+# Creating Arrays With np.random.rand()
+import numpy as np
+
+# create a 2D array of 2 rows and 2 columns of random numbers
+array1 = np.random.rand(2, 2)
+
+print("2-D Array: ")
+print(array1)
+
+# create a 3D array of shape (2, 2, 2) of random numbers
+array2 = np.random.rand(2, 2, 2)
+
+print("\n3-D Array: ")
+print(array2)
