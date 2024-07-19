0a42dec1b9f4d49893afb1980f05aedf2edc1962
file00 <51076009+file00@users.noreply.github.com>
2024-07-19 14:46:01 +0900

    Add files via upload

---
+++ Introduction/NumPy Array Creation/Create Array Using Python List.py
@@ -0,0 +1,12 @@
+# Create Array Using Python List
+import numpy as np
+
+# create a list named list1
+list1 = [2, 4, 6, 8]
+
+# create numpy array using list1
+array1 = np.array(list1)
+
+print(array1)
+
+# Output: [2 4 6 8]
---
+++ Introduction/NumPy Array Creation/Create an Array Using np.zeros().py
@@ -0,0 +1,9 @@
+# Create an Array Using np.zeros()
+import numpy as np
+
+# create an array with 4 elements filled with zeros
+array1 = np.zeros(4)
+
+print(array1)
+
+# Output: [0. 0. 0. 0.]
